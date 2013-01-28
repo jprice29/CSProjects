7696237e913810bfb246da939239af2377de3180
Elizabeth <techichan@gmail.com>
2013-01-28 12:17:04 -0600

    Example file

---
+++ AESEncryption/src/example.java
@@ -0,0 +1,22 @@
+
+/*
+ * This is a template of a java class.
+ */
+
+public class example {
+	
+	/*
+	 * The main method of the class
+	 */
+	public static void main(String[] args) {
+			helloWorld();
+	}
+	
+	/*
+	 * A function.
+	 */
+	public static void helloWorld() {
+		System.out.println("Hello World!");
+	}
+
+}
